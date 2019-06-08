f0d65b54ac0283ce01d5877985435101e7a09851
Rafal Labisz <rafal0710@gmail.com>
2019-06-08 12:37:40 +0200

    add atomic design

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -2,20 +2,20 @@ import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   padding: 0;
-  background-color: ${({ color }) => color || 'hsl(49, 100%, 58%)'};
+  background-color: ${({ theme }) => theme.primary};
   width: 220px;
   height: 47px;
   border: none;
   border-radius: 50px;
   font-family: 'Montserrat', sans-serif;
-  font-weight: 500;
+  font-weight: 600;
   font-size: 16px;
   text-transform: uppercase;
 
   ${({ secondary }) =>
     secondary &&
     css`
-      background-color: hsl(0, 0, 90%);
+      background-color: ${({ theme }) => theme.grey200};
       width: 105px;
       height: 30px;
       font-size: 10px;
