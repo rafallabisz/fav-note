edffacc7e1815890f8be363d9bc7cd05089a717e
Rafal Labisz <rafal0710@gmail.com>
2019-08-12 20:56:54 +0200

    fix display detailsPage

--- src/views/DetailsPage.js
+++ src/views/DetailsPage.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import DetailsTemplate from 'templates/DetailsTemplate';
 import withContext from 'hoc/withContext';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
-const DetailsPage = ({ activeItem }) => {
-  const [item] = activeItem;
+class DetailsPage extends Component {
+  state = {
+    activeItem: {
+      title: '',
+      content: '',
+      articleUrl: '',
+      twitterName: '',
+    },
+  };
 
-  return (
-    <DetailsTemplate
-      title={item.title}
-      created={item.created}
-      content={item.content}
-      articleUrl={item.articleUrl}
-      twitterName={item.twitterName}
-    />
-  );
-};
+  componentDidMount() {
+    if (this.props.activeItem) {
+      const [activeItem] = this.props.activeItem;
+      this.setState({ activeItem });
+    } else {
+      const { id } = this.props.match.params;
+      axios
+        .get(`http://localhost:9000/api/note/${id}`)
+        .then(({ data }) => {
+          this.setState({ activeItem: data });
+        })
+        .catch(err => console.log(err));
+    }
+  }
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <DetailsTemplate
+        title={activeItem.title}
+        content={activeItem.content}
+        articleUrl={activeItem.articleUrl}
+        twitterName={activeItem.twitterName}
+      />
+    );
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
-  return {
-    activeItem: state[ownProps.pageContext].filter(item => item._id === ownProps.match.params.id),
-  };
+  if (state[ownProps.pageContext]) {
+    return {
+      activeItem: state[ownProps.pageContext].filter(item => item._id === ownProps.match.params.id),
+    };
+  }
+  return {};
 };
 
 export default withContext(connect(mapStateToProps)(DetailsPage));
